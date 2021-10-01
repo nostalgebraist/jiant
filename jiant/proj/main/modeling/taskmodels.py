ce89189c0e19673dfe11bbb8a2fe2ac716e1dd62
nostalgebraist <nostalgebraist@gmail.com>
2021-10-01 14:22:58 -0700

    fix

--- jiant/proj/main/modeling/taskmodels.py
+++ jiant/proj/main/modeling/taskmodels.py
@@ -370,7 +370,7 @@ class ElmoStyleGPTClassificationModel(Taskmodel):
         super().__init__(task=task, encoder=encoder, head=head)
         print(kwargs)
         self.layer = kwargs["taskmodel_kwargs"]["layer"]
-        self.output_name = f"encoder.h.{self.layer}"
+        self.output_name = f"h.{self.layer}"
         for param in encoder.parameters():
             param.requires_grad = False
 
