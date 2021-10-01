bc079980704cae7712793e86a78b4a5a933fb887
nostalgebraist <nostalgebraist@gmail.com>
2021-10-01 11:57:06 -0700

    need this too

--- jiant/tasks/retrieval.py
+++ jiant/tasks/retrieval.py
@@ -32,7 +32,7 @@ from jiant.tasks.lib.mlm_pretokenized import MLMPretokenizedTask
 from jiant.tasks.lib.mlqa import MlqaTask
 from jiant.tasks.lib.mnli import MnliTask
 from jiant.tasks.lib.mnli_mismatched import MnliMismatchedTask
-from jiant.tasks.lib.mrpc import MrpcTask
+from jiant.tasks.lib.mrpc import MrpcTask, DevlinStyleMrpcTask
 from jiant.tasks.lib.mrqa_natural_questions import MrqaNaturalQuestionsTask
 from jiant.tasks.lib.multirc import MultiRCTask
 from jiant.tasks.lib.mutual import MutualTask
@@ -129,6 +129,7 @@ TASK_DICT = {
     "mutual": MutualTask,
     "mutual_plus": MutualPlusTask,
     "mrpc": MrpcTask,
+    "mrpc_devlin": DevlinStyleMrpcTask,
     "mrqa_natural_questions": MrqaNaturalQuestionsTask,
     "ner": NerTask,
     "newsqa": NewsQATask,
