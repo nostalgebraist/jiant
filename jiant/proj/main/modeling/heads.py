0549e7c551aca78c53ca71e319a4c27bb3a35443
nostalgebraist <nostalgebraist@gmail.com>
2021-10-02 09:24:09 -0700

    work

--- jiant/proj/main/modeling/heads.py
+++ jiant/proj/main/modeling/heads.py
@@ -238,16 +238,15 @@ class ElmoStyleGPTClassificationHead(BaseHead):
         return final
 
     def forward(self, unpooled, tokens):
-        # x = self.ln(unpooled)
         attn_outs = [attn(ln(x_))[0] for attn, ln, x_ in zip(self.attns, self.lns, unpooled)]
-        print([a.shape for a  in attn_outs])
+        attn_outs = [self.select_at_last_token(a, tokens) for a in attn_outs]
+        # print([a.shape for a in attn_outs])
         x = torch.cat(attn_outs, dim=-1)
-        print(x.shape)
-        # x = self.attn(x)[0]
-        x = self.select_at_last_token(x, tokens)
-        print(x.shape)
+        # print(x.shape)
+        # x = self.select_at_last_token(x, tokens)
+        # print(x.shape)
         x = x + self.mlp(x)
-        print(x.shape)
+        # print(x.shape)
         logits = self.out_proj(x)
         return logits
 
